8013f4387caf7a5da7f6a3eb3dafdfc98a34a64c
Dmitrii.Korobkin <dmitrii.korobkin@ringcentral.com>
2022-11-13 18:56:24 +0400

    test

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // 2 пункт
+        int n = 5;
+        double f = 5.5;
+        System.out.println(n+f);
+        System.out.println(n*f);
+        System.out.println(n/f);
+        System.out.println(n-f);
+        // 1 пункт
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MIN_VALUE;
+        System.out.println(a-1);
+        System.out.println(a+1);
+        System.out.println(b+1);
+        System.out.println(b-1);
+        System.out.println(n>=f);
+        System.out.println(n==f);
+        System.out.println(n!=f);
+        System.out.println(n<=f);
+    }
+}
